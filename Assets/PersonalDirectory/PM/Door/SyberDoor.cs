639fcc397c6d9e4167a3520b8bb48f596894369f
박경륜 <parkrye@gmail.com>
2023-09-04 15:39:16 +0900

    feat: 문 개발중

--- Assets/PersonalDirectory/PM/Door/SyberDoor.cs
+++ Assets/PersonalDirectory/PM/Door/SyberDoor.cs
@@ -2,44 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SyberDoor : MonoBehaviour, Hitable, Hackingable
+namespace PM
 {
-    [SerializeField] int hp;
-    Animator animator;
-    private void Start()
+    public class SyberDoor : MonoBehaviour, Hitable, Hackingable
     {
-        animator = GetComponent<Animator>();
-        StartCoroutine(OpenDoor());
-    }
-
-    public IEnumerator Break()
-    {
-        Destroy(gameObject);
-        yield return null;
-    }
-
-    public IEnumerator HackingCheck(bool success)
-    {
-        if (success)
-            StartCoroutine(OpenDoor());
-        else
+        [SerializeField] int hp;
+        Animator animator;
+        private void Start()
         {
-            hp += 5;
+            animator = GetComponent<Animator>();
         }
-        yield return null;
-    }
 
-    public IEnumerator Hit(int damage)
-    {
-        hp -= damage;
-        if (hp <= 0)
-            StartCoroutine(Break());
-        yield return null;
-    }
+        public IEnumerator Break()
+        {
+            Destroy(gameObject);
+            yield return null;
+        }
 
-    IEnumerator OpenDoor()
-    {
-        animator.SetTrigger("Open");
-        yield return null;
+        public IEnumerator HackingCheck(bool success)
+        {
+            if (success)
+                StartCoroutine(OpenDoor());
+            else
+            {
+                hp += 5;
+            }
+            yield return null;
+        }
+
+        public IEnumerator Hit(int damage)
+        {
+            hp -= damage;
+            if (hp <= 0)
+                StartCoroutine(Break());
+            yield return null;
+        }
+
+        IEnumerator OpenDoor()
+        {
+            animator.SetTrigger("Open");
+            yield return null;
+        }
     }
 }
