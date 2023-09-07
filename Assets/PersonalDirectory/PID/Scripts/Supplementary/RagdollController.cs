74a72ae62ef3c7e9fbd5a78e44f822ab0ca87b00
iopark <52244687+iopark@users.noreply.github.com>
2023-09-07 22:35:39 +0900

    DOCS: namespace rearranged.

--- Assets/PersonalDirectory/PID/Scripts/Supplementary/RagdollController.cs
+++ Assets/PersonalDirectory/PID/Scripts/Supplementary/RagdollController.cs
@@ -5,129 +5,132 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
-public class RagdollController : MonoBehaviour
+namespace PID
 {
-    int aliveLayer;
-    int deadLayer;
-    [Header("FSMProperties")]
-    BaseEnemy bodyOwner; 
-    [SerializeField] NavMeshAgent agent;
-    [SerializeField] Collider[] actionColliders;
-    Animator anim; 
-
-    [Header("RagdollProperties")]
-    [SerializeField] public Rigidbody[] ragdollBodies;
-    [SerializeField] CapsuleCollider[] ragdollColliders;
-    [SerializeField] CharacterJoint[] ragdollJoints;
-    [SerializeField] BoxCollider[] ragdollBox;
-    [SerializeField] SphereCollider ragdollHead;
-
-    [Header("RagDollParty")]
-    [SerializeField] float impactIntensity; 
-    private void Awake()
+    public class RagdollController : MonoBehaviour
     {
-        anim = GetComponent<Animator>();
-        bodyOwner = GetComponent<GuardEnemy>(); 
-        SearchComponentInChildren();
-        DisableRagdollProperties();
-        aliveLayer = LayerMask.GetMask("Enemy");
-        deadLayer = LayerMask.GetMask("EnemyNeutralized");
-    }
+        int aliveLayer;
+        int deadLayer;
+        [Header("FSMProperties")]
+        BaseEnemy bodyOwner;
+        [SerializeField] NavMeshAgent agent;
+        [SerializeField] Collider[] actionColliders;
+        Animator anim;
 
-    private void OnEnable()
-    {
-        bodyOwner.onDeath += DeathAftermath; 
-        bodyOwner.OnAndOff += OnAndOff;
-    }
+        [Header("RagdollProperties")]
+        [SerializeField] public Rigidbody[] ragdollBodies;
+        [SerializeField] CapsuleCollider[] ragdollColliders;
+        [SerializeField] CharacterJoint[] ragdollJoints;
+        [SerializeField] BoxCollider[] ragdollBox;
+        [SerializeField] SphereCollider ragdollHead;
 
-    private void OnDisable()
-    {
-        bodyOwner.onDeath -= DeathAftermath;
-        bodyOwner.OnAndOff -= OnAndOff;
-    }
-
-    private void SearchComponentInChildren()
-    {
-        ragdollColliders = GetComponentsInChildren<CapsuleCollider>();
-        ragdollJoints = GetComponentsInChildren<CharacterJoint>();
-        ragdollBodies = GetComponentsInChildren<Rigidbody>();
-    }
-
-    private void DisableRagdollProperties()
-    {
-        for (int i = 0; i < ragdollBodies.Length; i++)
+        [Header("RagDollParty")]
+        [SerializeField] float impactIntensity;
+        private void Awake()
         {
-            ragdollBodies[i].isKinematic = true; 
+            anim = GetComponent<Animator>();
+            bodyOwner = GetComponent<GuardEnemy>();
+            SearchComponentInChildren();
+            DisableRagdollProperties();
+            aliveLayer = LayerMask.GetMask("Enemy");
+            deadLayer = LayerMask.GetMask("EnemyNeutralized");
         }
-        for (int i = 0; i < ragdollColliders.Length; i++)
-        {
-            if (i == 0)
-                continue;
-            ragdollColliders[i].enabled = false;
-        }
-        for (int i = 0; i <ragdollBox.Length; i++ ) 
-        {
-            if (ragdollBox[i].isTrigger)
-                continue;
-            ragdollBox[i].enabled = false; 
-        }
-    }
 
-    private void EnableRagDollProperties()
-    {
-        for (int i = 0; i < ragdollBodies.Length; i++)
+        private void OnEnable()
         {
-            ragdollBodies[i].isKinematic = false;
+            bodyOwner.onDeath += DeathAftermath;
+            bodyOwner.OnAndOff += OnAndOff;
         }
-        for (int i = 0; i < ragdollColliders.Length; i++)
-        {
-            ragdollColliders[i].enabled = true;
-        }
-        for (int i = 0; i < ragdollBox.Length; i++)
-        {
-            if (ragdollBox[i].isTrigger)
-                continue;
-            ragdollJoints[i].connectedBody = null;
-        }
-    }
 
-    public void DeathAftermath(Vector3 hitDir,Vector3 hitPoint)
-    {
-        //Find 
-        Rigidbody hitPart = RobotHelper.NearestHitPart(ragdollBodies, hitPoint); 
-        StartCoroutine(DeathSimulation(hitDir, hitPoint, hitPart));
-    }
-
-    IEnumerator DeathSimulation(Vector3 hitDir, Vector3 hitPoint, Rigidbody impactPoint)
-    {
-        EnableRagDollProperties(); 
-        yield return new WaitForEndOfFrame();
-        impactPoint.AddForceAtPosition(-1 * impactIntensity * hitDir, hitPoint, ForceMode.Impulse);
-        //Find the nearest rigidbody with hitpoint 
-
-    }
-
-    
-    public void OnAndOff(bool switchOn)
-    {
-        if (switchOn)
+        private void OnDisable()
         {
-            SetGameLayerRecursive(gameObject, aliveLayer); 
-            anim.enabled = true;
-            anim.SetTrigger("Revive");
+            bodyOwner.onDeath -= DeathAftermath;
+            bodyOwner.OnAndOff -= OnAndOff;
         }
-        else
+
+        private void SearchComponentInChildren()
         {
-            SetGameLayerRecursive(gameObject, deadLayer);
-            anim.enabled = false;
+            ragdollColliders = GetComponentsInChildren<CapsuleCollider>();
+            ragdollJoints = GetComponentsInChildren<CharacterJoint>();
+            ragdollBodies = GetComponentsInChildren<Rigidbody>();
         }
-    }
-    private void SetGameLayerRecursive(GameObject gameObject, int layer)
-    {
-        gameObject.layer = layer;
-        foreach (Transform child in gameObject.transform)
+
+        private void DisableRagdollProperties()
         {
-            SetGameLayerRecursive(child.gameObject, layer);
+            for (int i = 0; i < ragdollBodies.Length; i++)
+            {
+                ragdollBodies[i].isKinematic = true;
+            }
+            for (int i = 0; i < ragdollColliders.Length; i++)
+            {
+                if (i == 0)
+                    continue;
+                ragdollColliders[i].enabled = false;
+            }
+            for (int i = 0; i < ragdollBox.Length; i++)
+            {
+                if (ragdollBox[i].isTrigger)
+                    continue;
+                ragdollBox[i].enabled = false;
+            }
+        }
+
+        private void EnableRagDollProperties()
+        {
+            for (int i = 0; i < ragdollBodies.Length; i++)
+            {
+                ragdollBodies[i].isKinematic = false;
+            }
+            for (int i = 0; i < ragdollColliders.Length; i++)
+            {
+                ragdollColliders[i].enabled = true;
+            }
+            for (int i = 0; i < ragdollBox.Length; i++)
+            {
+                if (ragdollBox[i].isTrigger)
+                    continue;
+                ragdollJoints[i].connectedBody = null;
+            }
+        }
+
+        public void DeathAftermath(Vector3 hitDir, Vector3 hitPoint)
+        {
+            //Find 
+            Rigidbody hitPart = RobotHelper.NearestHitPart(ragdollBodies, hitPoint);
+            StartCoroutine(DeathSimulation(hitDir, hitPoint, hitPart));
+        }
+
+        IEnumerator DeathSimulation(Vector3 hitDir, Vector3 hitPoint, Rigidbody impactPoint)
+        {
+            EnableRagDollProperties();
+            yield return new WaitForEndOfFrame();
+            impactPoint.AddForceAtPosition(-1 * impactIntensity * hitDir, hitPoint, ForceMode.Impulse);
+            //Find the nearest rigidbody with hitpoint 
+
+        }
+
+
+        public void OnAndOff(bool switchOn)
+        {
+            if (switchOn)
+            {
+                SetGameLayerRecursive(gameObject, aliveLayer);
+                anim.enabled = true;
+                anim.SetTrigger("Revive");
+            }
+            else
+            {
+                SetGameLayerRecursive(gameObject, deadLayer);
+                anim.enabled = false;
+            }
+        }
+        private void SetGameLayerRecursive(GameObject gameObject, int layer)
+        {
+            gameObject.layer = layer;
+            foreach (Transform child in gameObject.transform)
+            {
+                SetGameLayerRecursive(child.gameObject, layer);
+            }
         }
     }
 }
