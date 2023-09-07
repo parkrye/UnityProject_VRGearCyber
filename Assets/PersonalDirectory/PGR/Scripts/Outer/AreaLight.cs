9a7cf9e3077302b41204f85ad4fe8104f25f3308
parkrye <parkrye@gmail.com>
2023-09-08 06:36:31 +0900

    feat: Player Apartment Door

---
+++ Assets/PersonalDirectory/PGR/Scripts/Outer/AreaLight.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PGR
+{
+    public class AreaLight : MonoBehaviour
+    {
+        [SerializeField] Light[] lights;
+        [SerializeField] int isPlayerInArea;
+
+        void Awake()
+        {
+            lights = GetComponentsInChildren<Light>();
+
+            foreach (Light light in lights)
+            {
+                light.enabled = false;
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag.Equals("Player"))
+            {
+                isPlayerInArea++;
+                if(isPlayerInArea > 0)
+                {
+                    foreach(Light light in lights)
+                    {
+                        light.enabled = true;
+                    }
+                }
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.tag.Equals("Player"))
+            {
+                isPlayerInArea--;
+                if (isPlayerInArea == 0)
+                {
+                    foreach (Light light in lights)
+                    {
+                        light.enabled = false;
+                    }
+                }
+            }
+        }
+    }
+
+}
